44139bb14bb43620b35165ac3a926b4b6e5fc117
pratamawijaya <set.mnemonix@gmail.com>
2021-04-12 11:15:31 +0700

    simple blinking led

---
+++ esp32-blink/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define LED 2
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  delay(500);
+  digitalWrite(LED,HIGH);
+  delay(500);
+  digitalWrite(LED,LOW);
+}
