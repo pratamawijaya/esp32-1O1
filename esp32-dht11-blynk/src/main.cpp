83f0e6b0b3c863a8f5c070f73cfea252ce99d27b
pratamawijaya <set.mnemonix@gmail.com>
2021-04-13 15:34:02 +0700

    update, add oled display

--- esp32-dht11-blynk/src/main.cpp
+++ esp32-dht11-blynk/src/main.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
-#include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <Adafruit_Sensor.h>
-#include "DHT.h"
+#include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+String temperature;
+String humidity;
+String pressure;
+
+unsigned long previousMillis = 0;
+const long interval = 5000;
 
 uint8_t LED = 2;
-char auth[] = "xxxx";
+char auth[] = "xxx";
 char ssid[] = "rahasia";
 char pass[] = "12345678";
 
@@ -47,6 +63,33 @@ void readDHTSensor()
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
+  display.clearDisplay();
+  // display temperature
+   // display temperature
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("Temperature: ");
+  display.setTextSize(2);
+  display.setCursor(0,10);
+  display.print(t);
+  display.print(" ");
+  display.setTextSize(1);
+  display.cp437(true);
+  display.write(167);
+  display.setTextSize(2);
+  display.print("C");
+
+  // display humidity
+  display.setTextSize(1);
+  display.setCursor(0, 35);
+  display.print("Humidity: ");
+  display.setTextSize(2);
+  display.setCursor(0, 45);
+  display.print(h);
+  display.print(" %"); 
+
+  display.display();
+
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
@@ -71,13 +114,25 @@ void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+
   pinMode(LED, OUTPUT);
   dht.begin();
 
   // Setup a function to be called every second
   timer.setInterval(5000L, readDHTSensor);
 
-  Blynk.begin("xxxxx", ssid, pass);
+  Blynk.begin("xxx", ssid, pass);
 }
 
 void loop()
